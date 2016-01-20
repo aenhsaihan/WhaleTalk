573e9cf802b1bdd88bdb152fef42e92ff4b190c2
Anar Enhsaihan <anar.enhsaihan@gobutler.com>
2016-01-20 13:38:47 -0500

    Prepare table view for data source

---
+++ WhaleTalk/ChatViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  WhaleTalk
+//
+//  Created by Anar Enhsaihan on 1/20/16.
+//  Copyright © 2016 nomad. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+    
+    private let tableView = UITableView()
+    
+    private var messages = [Message]()
+    private let cellIdentifier = "Cell"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        
+        let tableViewConstraints: [NSLayoutConstraint] = [
+            
+            tableView.topAnchor.constraintEqualToAnchor(view.topAnchor),
+            tableView.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor),
+            tableView.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor),
+            tableView.bottomAnchor.constraintEqualToAnchor(view.bottomAnchor)
+        ]
+        
+        NSLayoutConstraint.activateConstraints(tableViewConstraints)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ WhaleTalk/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  WhaleTalk
+//
+//  Created by Anar Enhsaihan on 1/20/16.
+//  Copyright © 2016 nomad. All rights reserved.
+//
+
+import Foundation
+
+class Message {
+    var text : String?
+}
