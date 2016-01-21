9ec6c3b536f40f40700c9ee701d481bed30ed4aa
Anar Enhsaihan <anar.enhsaihan@gobutler.com>
2016-01-21 14:57:48 -0500

    Register our custom cell with tableview

--- WhaleTalk/ChatViewController.swift
+++ WhaleTalk/ChatViewController.swift
@@ -25,7 +25,7 @@ class ChatViewController: UIViewController {
             messages.append(m)
         }
         
-        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        tableView.registerClass(ChatCell.self, forCellReuseIdentifier: cellIdentifier)
         tableView.dataSource = self
         
         tableView.translatesAutoresizingMaskIntoConstraints = false
@@ -58,10 +58,10 @@ extension ChatViewController : UITableViewDataSource {
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! ChatCell
         
         let message = messages[indexPath.row]
-        cell.textLabel?.text = message.text
+        cell.messageLabel.text = message.text
         
         return cell
     }
